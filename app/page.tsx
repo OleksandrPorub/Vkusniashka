6f4aad6bf0a0ec400478d46fc7ce1a641022716f
OleksandrPorub <oleksandr.porub@gmail.com>
2023-12-25 19:53:50 +0200

    designing

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import styles from "./home.module.scss";
 
 export default function Home() {
     return (
-        <div className="page">
-            <article className={styles.intro}>
-                <h2>ВКУСНЯШКИ ВІД НАТАШКИ</h2>
-                <p className={styles.description}>Магазин смачної домашньої кухні</p>
-            </article>
+        <div className={"page"}>
+            <div className={styles.wrapper}>
+                <article className={styles.intro}>
+                    <h2>ВКУСНЯШКИ ВІД НАТАШКИ</h2>
+                    <p className={styles.description}>Магазин смачної домашньої кухні</p>
+                </article>
+            </div>
         </div>
     );
 }

460bc3cf3dfc4a95cd99e93b2a13ffdfe169d303
OleksandrPorub <oleksandr.porub@gmail.com>
2024-01-08 23:46:23 +0200

    added transition

--- app/components/signInPopUp/TheSignInPopUp.tsx
+++ app/components/signInPopUp/TheSignInPopUp.tsx
@@ -6,7 +6,7 @@ import { signIn } from "next-auth/react";
 import TheLoader from "../UI/loader/TheLoader";
 import { useRouter, useSearchParams } from "next/navigation";
 import TheCloserIcon from "../UI/closerIcon/TheCloserIcon";
-import { AnimatePresence, motion } from "framer-motion";
+// import { AnimatePresence, motion } from "framer-motion";
 
 const TheSignInPopUp = () => {
     const searchParams = useSearchParams();
@@ -14,11 +14,17 @@ const TheSignInPopUp = () => {
     const router = useRouter();
 
     const [isLoading, setIsLoading] = useState(false);
-    // const [isFormVisible, setIsFormVisible] = useState(false);
 
-    // useEffect(()=>{
-    //     setIsFormVisible(true);
-    // },[])
+    useEffect(() => {
+        const elem_underlay = document.getElementsByClassName(styles.underlay)[0];
+        const elem_content = document.getElementsByClassName(styles.content)[0];
+        if (elem_underlay) {
+            elem_underlay.classList.add(styles.appear);
+        }
+        if (elem_content) {
+            elem_content.classList.add(styles.appear);
+        }
+    }, []);
 
     const closer = () => {
         router.push(callbackUrl);
@@ -40,66 +46,62 @@ const TheSignInPopUp = () => {
     };
 
     return (
-        <AnimatePresence>
-            <motion.div
-                className={styles.underlay}
-                initial={{ opacity: 0 }}
-                animate={{
-                    opacity: 1,
-                }}
-                transition={{
-                    duration: 0.5,
-                    opacity: 1,
-                    type: "linear",
-                }}
-            >
-                {
-                    <motion.div
-                        className={styles.content}
-                        initial={{ y: -100, opacity: 0.6 }}
-                        animate={{
-                            y: 0,
-                            opacity: 1,
-                        }}
-                        transition={{
-                            duration: 0.5,
-                            opacity: 1,
-                            type: "spring",
+        <div
+            className={styles.underlay}
+            // initial={{ opacity: 0 }}
+            // animate={{
+            //     opacity: 1,
+            // }}
+            // transition={{
+            //     duration: 0.5,
+            //     ease: "linear",
+            // }}
+        >
+            {
+                <div
+                    className={styles.content}
+                    // initial={{ y: -200 }}
+                    // animate={{
+                    //     y: 0,
+                    // }}
+                    // transition={{
+                    //     duration: 0.7,
+                    //     type: "spring",
+                    //     bounce: 0.4,
+                    // }}
+                >
+                    <button
+                        className={styles.btn_closerIcon}
+                        onClick={() => {
+                            closer();
                         }}
                     >
-                        <button
-                            className={styles.btn_closerIcon}
-                            onClick={() => {
-                                closer();
-                            }}
-                        >
-                            <TheCloserIcon width={35} color={"white"}></TheCloserIcon>
-                        </button>
+                        <TheCloserIcon width={35} color={"white"}></TheCloserIcon>
+                    </button>
 
-                        <h3 className={styles.title}>
-                            Якщо ви адміністратор - введіть облікові дані, <br /> інакше - закрийте це вікно, натиснувши на перехрестя.
-                        </h3>
-                        <form onSubmit={handleSubmit} className={styles.form}>
-                            <label>
-                                логін:
-                                <input name="login" type="text" />
-                            </label>
-                            <label>
-                                пароль:
-                                <input name="password" type="text"></input>
-                            </label>
-                            {isLoading ? (
-                                <TheLoader></TheLoader>
-                            ) : (
-                                <button className={styles.btn_submit} type="submit">
-                                    авторизуватись
-                                </button>
-                            )}
-                        </form>
-                    </motion.div>
-                }
-            </motion.div>
-        </AnimatePresence>
+                    <h3 className={styles.title}>
+                        Якщо ви адміністратор - введіть облікові дані, <br /> інакше - закрийте це вікно, натиснувши на перехрестя.
+                    </h3>
+                    <form onSubmit={handleSubmit} className={styles.form}>
+                        <label>
+                            логін:
+                            <input name="login" type="text" />
+                        </label>
+                        <label>
+                            пароль:
+                            <input name="password" type="text"></input>
+                        </label>
+                        {isLoading ? (
+                            <TheLoader></TheLoader>
+                        ) : (
+                            <button className={styles.btn_submit} type="submit">
+                                авторизуватись
+                            </button>
+                        )}
+                    </form>
+                </div>
+            }
+        </div>
     );
 };
 

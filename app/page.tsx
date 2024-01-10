cb0396f0b078e7e34da67d319a6f5ea371d3251c
OleksandrPorub <oleksandr.porub@gmail.com>
2024-01-11 01:04:12 +0200

    added big logo to the intro and background replacement

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import styles from "./home.module.scss";
+import imgLogoSrc from "../public/images/vkusniaski-logo.png";
 
 export default function Home() {
     return (
         <div className={"page"}>
             <div className={styles.wrapper}>
                 <article className={styles.intro}>
+                    <div className={styles.logoWrapper}>
+                    <Image className={styles.imgLogo} src={imgLogoSrc} width={600} height={393} alt="logo"></Image>
+
+                    </div>
                     <h2>ВКУСНЯШКИ ВІД НАТАШКИ</h2>
                     <p className={styles.description}>Магазин смачної домашньої кухні</p>
                 </article>

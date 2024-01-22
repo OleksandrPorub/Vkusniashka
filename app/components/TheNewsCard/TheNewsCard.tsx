1f96698822b9ba8cbbd54291dab861fc479c7611
OleksandrPorub <oleksandr.porub@gmail.com>
2024-01-22 03:58:43 +0200

    changed buttons` disign

--- app/components/TheNewsCard/TheNewsCard.tsx
+++ app/components/TheNewsCard/TheNewsCard.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import SvgIcon_Del from "../svg/SvgIcon_Del";
+import SvgIcon_Edit from "../svg/SvgIcon_Edit";
 import styles from "./TheNewsCard.module.scss";
-// import { useEffect, useRef, useState } from "react";
 import { Session } from "next-auth";
 
 type newsType = {
@@ -15,69 +16,49 @@ type PropsType = {
     setActiveNews: Function;
     dateNow: Date;
     itemDeletion: Function;
-    session: Session|null;
+    session: Session | null;
 };
 
 const daysOfTheWeek = ["неділя", "понеділок", "вівторок", "середа", "четвер", "п'ятниця", "субота"];
 
 const TheNewsCard = ({ news, setActiveNews, dateNow, itemDeletion, session }: PropsType) => {
-    
-    // const [newsCardTextElement, setNewsCardTextElement]: [Element | null, Function] = useState(null);
-
+  
     const whichDay = (date: number) => {
         if (new Date(date).toLocaleDateString() === dateNow.toLocaleDateString()) return "СЬОГОДНІ";
         if (new Date(date + 24 * 3600 * 1000).toLocaleDateString() === dateNow.toLocaleDateString()) return "ВЧОРА";
         return null;
     };
-    // const newsCardTextRef = useRef(null);
-
-    // useEffect(() => {
-    //     const elementText = newsCardTextRef.current;
-    //     console.log(elementText);
-    //     setNewsCardTextElement(elementText);
-    //     if (elementText) {
-    //         setNewsCardTextElement(elementText);
-    //     }
-    // }, []);
-
-    // useEffect(() => {
-    //     console.dir(newsCardTextElement);
-    //     if (newsCardTextElement) {
-    //         newsCardTextElement.innerText = news.text || "";
-    //     }
-    // }, [news, newsCardTextElement]);
 
     return (
-        <article className={styles.newsCard}>
-            {session && (
-                <div className={styles.adminSection}>
-                    <button
-                        className={styles.adminBtn}
-                        onClick={() => {
-                            setActiveNews(news);
-                        }}
-                    >
-                        edit
-                    </button>
-                    <button
-                        className={styles.adminBtn}
-                        onClick={() => {
-                            itemDeletion(news.id);
-                        }}
-                    >
-                        del
-                    </button>
-                </div>
-            )}
-            <span className={styles.newsCard_date}>
-                <strong className={styles.newsCard_date_strong}>{whichDay(news.date)}</strong>&nbsp;&nbsp;
-                {new Date(news.date).toLocaleDateString()}&nbsp;&nbsp;{daysOfTheWeek[new Date(news.date).getDay()]}
-            </span>
-            <h4 className={styles.newsCard_title}>{news.title}</h4>
-            <pre className={styles.newsCard_text}>
-                {news.text || ""}
-            </pre>
-        </article>
+            <article className={styles.newsCard}>
+                {session && (
+                    <div className={styles.adminSection}>
+                        <button
+                            className={styles.adminBtn}
+                            onClick={() => {
+                                setActiveNews(news);
+                            }}
+                        >
+                            <SvgIcon_Edit/>
+                        </button>
+                        <button
+                            className={styles.adminBtn}
+                            onClick={() => {
+                                window.confirm("Видалити новину назавжди?") && itemDeletion(news.id);
+                            }}
+                        >
+                            <SvgIcon_Del/>
+                        </button>
+                    </div>
+                )}
+                <span className={styles.newsCard_date}>
+                    <strong className={styles.newsCard_date_strong}>{whichDay(news.date)}</strong>&nbsp;&nbsp;
+                    {new Date(news.date).toLocaleDateString()}&nbsp;&nbsp;{daysOfTheWeek[new Date(news.date).getDay()]}
+                </span>
+                <h4 className={styles.newsCard_title}>{news.title}</h4>
+                <pre className={styles.newsCard_text}>{news.text || ""}</pre>
+            </article>
+        // </div>
     );
 };
 
--- app/components/productListVisitor/TheProductListVisitor.tsx
+++ app/components/productListVisitor/TheProductListVisitor.tsx
@@ -3,7 +3,7 @@ import styles from "./TheProductListVisitor.module.scss";
 import { productItemType } from "@/app/data/assortment";
 
 type propsType = {
-    productList: productItemType[];  
+    productList: productItemType[];
 };
 
 const TheProductListVisitor: FC<propsType> = ({ productList }) => {
@@ -13,17 +13,14 @@ const TheProductListVisitor: FC<propsType> = ({ productList }) => {
                 <div className={styles.listHeaderProp + " " + styles.head_num}>№</div>
                 <div className={styles.listHeaderProp + " " + styles.head_name}>Найменування</div>
                 <div className={styles.listHeaderProp + " " + styles.head_quantity}>Вихід продукту</div>
-                <div className={styles.listHeaderProp + " " + styles.head_price}>Ціна <br/>грн</div>
+                <div className={styles.listHeaderProp + " " + styles.head_price}>
+                    Ціна <br />
+                    грн
+                </div>
             </div>
             <ul className={styles.productsList}>
                 {productList.map((product: productItemType, i) => (
-                    <li
-                        className={
-                            styles.productItem
-                        }
-                     
-                        key={product.id}
-                    >
+                    <li className={styles.productItem} key={product.id}>
                         <section className={styles.section_number}>
                             <div className={styles.productProp + " " + styles.prop_num}>{i + 1}</div>
                         </section>
@@ -32,7 +29,16 @@ const TheProductListVisitor: FC<propsType> = ({ productList }) => {
                                 <div className={styles.productProp + " " + styles.prop_name}>{product.name}</div>
                                 <div className={styles.productProp + " " + styles.prop_quantity}>{product.quantity}</div>
                                 <div className={styles.productProp + " " + styles.prop_price}>
-                                    {product.price ? product.price.toFixed(2) : "-------"}
+                                    {/* {product.price ? product.price.toFixed(2) : "-------"} */}
+                                    {product.price ? (
+                                        <>
+                                            <span className={styles.price_integer}>{product.price.toFixed()}</span>
+                                             .
+                                            <span  className={styles.price_fraction}>{product.price.toFixed(2).split('.')[1]}</span>
+                                        </>
+                                    ) : (
+                                        "-------"
+                                    )}
                                 </div>
                             </section>
                             <section className={styles.section_description}>
